afc76337693a0e3cab454f3cac163786f4e1a8fc
Meow <vijay.sirohi2022@gmail.com>
2025-06-14 05:37:20 +0000

    Added day8

---
+++ day8/health-report.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#Output file
+report="system-report-$(date +%Y%m%d).txt"
+
+echo "System Health Report ----- $(date)" > $report
+echo "--------------------------------" >> $report
+
+#CPU Usage
+echo "----CPU USAGE----" >> $report
+top -bn1 | grep "Cpu(s)" >> $report
+
+#RAM Usage
+echo "----RAM USAGE----" >> $report
+free -m >> $report
+
+#DISK Usage
+echo "----DISK USAGE----" >> $report
+df -h >> $report
+
+#Who is logged in
+echo "Logged in Users:" >> $report
+who >> $report
+
+echo "Report generated" >> $report
+
+
